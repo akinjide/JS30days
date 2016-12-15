242dc2ec1492482a54ea3a7c3b2dff69c01e46a0
Akinjide Bankole <r@akinjide.me>
2016-12-15 11:16:59 +0100

    initial commit :)

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const jade = require('jade')
+const app = express()
+
+app.set('view engine', 'jade')
+app.set('port', process.env.PORT || 3000)
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res) {
+  res.render('help.jade')
+})
+
+app.get('/:path', function(req, res) {
+  res.render(`${req.params.path}.jade`)
+})
+
+app.listen(app.get('port'));
+console.log('stuff happening ¯\_(ツ)_/¯ *:', app.get('port'))
